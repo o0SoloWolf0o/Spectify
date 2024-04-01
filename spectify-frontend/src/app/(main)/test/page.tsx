61fbc2670b07e4f714ec1be1c7aad3501132967e
Pumipat <88040178+o0SoloWolf0o@users.noreply.github.com>
2024-04-01 14:55:50 +0700

    [feet] product page (product detail)

--- spectify-frontend/src/app/(main)/test/page.tsx
+++ spectify-frontend/src/app/(main)/test/page.tsx
@@ -122,65 +122,10 @@ type Product = cpuProducts | ramProducts | gpuProducts | moboProducts | hddProdu
 
 
 export default function App() {
-    const { isOpen, onOpen, onOpenChange } = useDisclosure();
-
 
 
     return (
         <>
-            <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-                <div style={{ display: 'flex', flexDirection: 'column', flex: 1 }}>
-                    <h1>title</h1>
-                    <p style={{ fontSize: '13px' }}>brand</p>
-                    <Image
-                        src="/spectify-frontend/public/images/Logo.png"
-                        alt="Product"
-                        width={200}
-                        height={200}
-                        style={{ boxShadow: '2px 4px 8px rgba(0, 0, 0, 0.1)' }}
-                    />
-                    <div style={{ marginTop: '7px' }}>
-                        <p style={{ color: '#6B6B6B' }}>Estimate Price</p>
-                        <p style={{ fontWeight: 'bold' }}>price THB</p>
-                    </div>
-                </div>
-                <div style={{ background: "#DCF1FB", flex: 1, padding: '20px' }}>
-                    <h1>description</h1>
-                </div>
-            </div>
-
-            <Button onPress={onOpen}>Open Modal</Button>
-            <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
-                <ModalContent>
-                    {(onClose) => (
-                        <>
-                            <ModalHeader className="flex flex-col gap-1">Modal Title</ModalHeader>
-                            <ModalBody>
-                                <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-                                    <div style={{ display: 'flex', flexDirection: 'column', flex: 1 }}>
-                                        <h1>title</h1>
-                                        <p style={{ fontSize: '13px' }}>brand</p>
-                                        <Image
-                                            src="/spectify-frontend/public/images/Logo.png"
-                                            alt="Product"
-                                            width={200}
-                                            height={200}
-                                            style={{ boxShadow: '2px 4px 8px rgba(0, 0, 0, 0.1)' }}
-                                        />
-                                        <div style={{ marginTop: '7px' }}>
-                                            <p style={{ color: '#6B6B6B' }}>Estimate Price</p>
-                                            <p style={{ fontWeight: 'bold' }}>price THB</p>
-                                        </div>
-                                    </div>
-                                    <div style={{ background: "#DCF1FB", flex: 1, marginLeft: '20px', padding: '20px' }}>
-                                        <h1>description</h1>
-                                    </div>
-                                </div>
-                            </ModalBody>
-                        </>
-                    )}
-                </ModalContent>
-            </Modal>
 
 
         </>
