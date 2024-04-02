a6e0d5e7c7e07480f4a48dc211847f4d162d7f2f
Pumipat <88040178+o0SoloWolf0o@users.noreply.github.com>
2024-04-02 22:57:56 +0700

    [fix] conflict

--- spectify-frontend/src/components/ui/buildPage-Component/build-opti.tsx
+++ spectify-frontend/src/components/ui/buildPage-Component/build-opti.tsx
@@ -8,7 +8,7 @@ const Optimization = () => {
     
     return (
         <>
-            wwwww
+            
         </>
     );
 };
--- spectify-frontend/src/components/ui/buildPage-Component/build-performance.tsx
+++ spectify-frontend/src/components/ui/buildPage-Component/build-performance.tsx
@@ -6,7 +6,7 @@ const Performance = () => {
     
     return (
         <>
-        wwwwwww
+        
         </>
     );
 };
