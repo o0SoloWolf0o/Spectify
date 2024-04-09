ed5f6ea0c2b6703c74fa5339f3dc2f19d4fae681
Thalit Chaimunin <112146489+time1568@users.noreply.github.com>
2024-04-09 15:05:27 +0700

    Visualized Image v1

--- spectify-frontend/src/components/ui/buildPage-Component/build-visualization.tsx
+++ spectify-frontend/src/components/ui/buildPage-Component/build-visualization.tsx
@@ -7,14 +7,14 @@ type ProductImageMap = { [key in keyof SelectedProducts]: string };
 
 const productTypeToImageUrl: ProductImageMap = {  
 
-  CPU: 'https://www.nextstepreborn.co.th/wp-content/uploads/2022/01/cover-i5-6500-png-resize.png',
-  GPU: 'https://www.asus.com/media/Odin/Websites/global/ProductLine/20201028111930.png',
-  MB: 'https://png.pngtree.com/png-vector/20230728/ourmid/pngtree-motherboard-clipart-an-image-of-a-motherboard-cartoon-vector-png-image_6814261.png',
-  RAM: 'https://i.pinimg.com/originals/d9/26/92/d926929a407749d1e1a259193bc74885.png',
-  SSD: 'https://png.pngtree.com/png-vector/20230328/ourmid/pngtree-ssd-solid-drive-computer-storage-vector-png-image_6672882.png',
-  PSU: 'https://atlas-content-cdn.pixelsquid.com/stock-images/computer-power-supply-pc-18d5exA-600.jpg',
-  Case: 'https://atlas-content-cdn.pixelsquid.com/stock-images/full-pc-case-open-desktop-computer-8JYmGv4-600.jpg',
-  Cooler: 'https://cdn-reichelt.de/bilder/web/xxl_ws/E200/BEQUIET_BK021-01.png',
+  CPU: '/images/vispng/CPU.png',
+  GPU: '/images/vispng/GPU.png',
+  MB: '/images/vispng/MB.png',
+  RAM: '/images/vispng/RAM.png',
+  SSD: '/images/vispng/SSD.png',
+  PSU: '/images/vispng/PSU.png',
+  Case: '/images/vispng/Case.png',
+  Cooler: '/images/vispng/Cooler.png',
 
 };
 
@@ -41,6 +41,7 @@ const Visualization: React.FC<VisualizationProps> = ({ selectedProducts }) => {
             src={imageUrl} 
             alt={type} 
             style={{ width: '50px', height: '50px' }} 
+            className='w-full h-full'
           />
           </div>
         );
