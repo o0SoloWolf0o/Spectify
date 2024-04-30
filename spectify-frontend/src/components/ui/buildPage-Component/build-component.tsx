8f18fc7cab395d48d0f9de8eacdf92d12c7cd4a3
Pumipat <88040178+o0SoloWolf0o@users.noreply.github.com>
2024-04-30 13:34:32 +0700

    Fix type casting issue in build-component.tsx

--- spectify-frontend/src/components/ui/buildPage-Component/build-component.tsx
+++ spectify-frontend/src/components/ui/buildPage-Component/build-component.tsx
@@ -26,18 +26,19 @@ const BuildComponent: React.FC<BuildComponentProps> = ({ selectedProducts, setSe
     const [selectedMoboRamType, setSelectedMoboRamType] = useState<string | null>(null);
 
     const handleSelectProduct = (typeProduct: string, product: Product) => {
-    
         setSelectedProducts(prevProducts => {
             const newProducts = { ...prevProducts, [typeProduct]: product };
             localStorage.setItem('selectedProducts', JSON.stringify(newProducts));
             localStorage.setItem(typeProduct, product.id);
-            
+    
             if (typeProduct === 'CPU' && 'socket' in product) {
-                setSelectedCpuSocket((product as cpuProducts).socket);
+                const cpuProduct = product as cpuProducts;
+                setSelectedCpuSocket(cpuProduct.socket);
             } else if (typeProduct === 'MB' && 'ramslot' in product) {
-                setSelectedMoboRamType((product as unknown as moboProducts).ramslot);
+                const moboProduct = product as unknown as moboProducts;
+                setSelectedMoboRamType(moboProduct.ramslot);
             }
-
+    
             return newProducts;
         });
     };
