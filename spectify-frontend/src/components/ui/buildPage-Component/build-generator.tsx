fd2492fe33c8b6ffd6a4e35e7f3a80bba530d194
Pumipat <88040178+o0SoloWolf0o@users.noreply.github.com>
2024-05-01 00:33:44 +0700

    Refactor variable names and import paths in productPage.tsx and build-generator.tsx

--- spectify-frontend/src/components/ui/buildPage-Component/build-generator.tsx
+++ spectify-frontend/src/components/ui/buildPage-Component/build-generator.tsx
@@ -1,9 +1,125 @@
 "use client";
 
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Switch } from "@nextui-org/react";
+import {
+    cpuProducts,
+    ramProducts,
+    gpuProducts,
+    moboProducts,
+    ssdProducts,
+    cpuCoolerProducts,
+} from "@/components/main/product/productPage";
+
+import { getCpuProducts, getRamProducts, getGpuProducts, getSsdProducts, getCpuCoolerProducts, getMoboProducts, getPsuProducts, getCaseComputersProducts } from "@/action/product";
+
+type caseComputerProducts = {
+    id: string;
+    typeProduct: string;
+    name: string;
+    image: string;
+    size: string;
+    isolation: string;
+    description: string;
+    price: string;
+}
+
+type psuProducts = {
+    id: string;
+    typeProduct: string;
+    name: string;
+    image: string;
+    wattage: string;
+    description: string;
+    price: string;
+    tdp: string;
+};
 
 const Generator = () => {
+
+    const [cpuProducts, setCPUProducts] = useState<cpuProducts[]>([]);
+    const [ramProducts, setRamProducts] = useState<ramProducts[]>([]);
+    const [gpuProducts, setGpuProducts] = useState<gpuProducts[]>([]);
+    const [moboProducts, setMoboProducts] = useState<moboProducts[]>([]);
+    const [ssdProducts, setSsdProducts] = useState<ssdProducts[]>([]);
+    const [cpuCoolerProducts, setCpuCoolerProducts] = useState<cpuCoolerProducts[]>([]);
+    const [psuProducts, setPsuProducts] = useState<psuProducts[]>([]);
+    const [caseComputerProducts, setCaseComputerProducts] = useState<caseComputerProducts[]>([]);
+
+    const fetchProducts = async () => {
+        try {
+            const [
+                cpuProducts,
+                ramProducts,
+                gpuProducts,
+                moboProducts,
+                ssdProducts,
+                cpuCoolerProducts,
+                psuProducts,
+                caseComputerProducts,
+            ] = await Promise.all([
+                getCpuProducts(),
+                getRamProducts(),
+                getGpuProducts(),
+                getMoboProducts(),
+                getSsdProducts(),
+                getCpuCoolerProducts(),
+                getPsuProducts(),
+                getCaseComputersProducts(),
+            ]);
+            setCPUProducts(cpuProducts);
+            setMoboProducts(moboProducts);
+            setRamProducts(ramProducts);
+            setGpuProducts(gpuProducts);
+            setSsdProducts(ssdProducts);
+            setPsuProducts(psuProducts);
+            setCaseComputerProducts(caseComputerProducts);
+            setCpuCoolerProducts(cpuCoolerProducts);
+        } catch (error) {
+            console.error("Error fetching products:", error);
+        }
+    };
+    fetchProducts();
+
+    // Select all products from all products prices make it lower than budget or = budget and save all product to local storage
+    const budget = 30000;
+    // cpuprice + ramprice + gpuprice + moboprice + ssdprice + cpucoolerprice + psuprice + caseprice
+    // cpuProducts.forEach(cpuProduct => {
+    //     console.log(cpuProduct.price);
+    // });
+    // ramProducts.forEach(ramProduct => {
+    //     console.log(ramProduct.price);
+    // });
+    // gpuProducts.forEach(gpuProduct => {
+    //     console.log(gpuProduct.price);
+    // });
+    // moboProducts.forEach(moboProduct => {
+    //     console.log(moboProduct.price);
+    // });
+    // ssdProducts.forEach(ssdProduct => {
+    //     console.log(ssdProduct.price);
+    // });
+    // cpuCoolerProducts.forEach(cpuCoolerProduct => {
+    //     console.log(cpuCoolerProduct.price);
+    // });
+    // psuProducts.forEach(psuProduct => {
+    //     console.log(psuProduct.price);
+    // });
+    // caseComputerProducts.forEach(caseComputerProduct => {
+    //     console.log(caseComputerProduct.price);
+    // });
+
+    // 
+
+    const saveToLocalStorage = (data: any[]) => {
+        try {
+            const jsonData = JSON.stringify(data);
+            localStorage.setItem("compareData", jsonData);
+            console.log("Data saved to local storage:", jsonData);
+        } catch (error) {
+            console.error("Error saving data to local storage:", error);
+        }
+    };
 
     const [isSwitchOn, setIsSwitchOn] = useState(false);
 
@@ -32,22 +148,6 @@ const Generator = () => {
                             min="0"
                             className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-full p-2.5 mb-5 mx-5'
                         />
-                    </div>
-                </div>
-
-                <div>
-                    <label className='mb-2 text-sm font-medium text-gray-900 dark:text-white'>
-                        <h3 className='text-xl font-semibold ml-5 mb-2'>Priority</h3>
-                    </label>
-                    <div className='flex justify-center'>
-                        <select
-                            className='flex bg-gray-50 border border-[#D9D9D9] focus:ring-[#00A9FF] focus:border-[#00A9FF] text-black text-md hover:cursor-pointer rounded-lg w-full mx-5 mb-5 p-2.5'
-                        >
-                            <option value='Gaming'>Gaming</option>
-                            <option value='Programming'>Programming</option>
-                            <option value='Video Editing'>Video Editing</option>
-                            <option value='Office working'>Office working</option>
-                        </select>
                     </div>
                 </div>
 
