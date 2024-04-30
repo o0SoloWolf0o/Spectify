b8384a430f0140afa6f6569e5557d5040852d406
nanthawatsk <jamesukkam@gmail.com>
2024-04-30 15:28:22 +0900

    feat/like

---
+++ spectify-frontend/src/database/like.ts
@@ -0,0 +1,48 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export const like = async (user_id: string, build_id: string) => {
+    await prisma.like.create({
+        data: {
+            user_id,
+            build_id,
+            date: new Date(),
+        },
+    });
+}
+
+export const unlike = async (user_id: string, build_id: string) => {
+    await prisma.like.deleteMany({
+        where: {
+            user_id,
+            build_id,
+        },
+    });
+};
+
+export const isLiked = async (user_id: string, build_id: string) => {
+    const like = await prisma.like.findFirst({
+        where: {
+            user_id,
+            build_id,
+        },
+    });
+    return like !== null;
+};
+
+export const getLikes = async (user_id: string) => {
+    return await prisma.like.findMany({
+        where: {
+            user_id,
+        },
+    });
+};
+
+export const getLikesCount = async (build_id: string) => {
+    return await prisma.like.count({
+        where: {
+            build_id,
+        },
+    });
+};
