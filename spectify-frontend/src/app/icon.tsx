b5d817fe8f57516cc7408a25133d8fe66d1fd9ea
Pumipat <88040178+o0SoloWolf0o@users.noreply.github.com>
2024-05-02 11:22:30 +0700

    Refactor Icon component to use arrow function syntax and add image generation logic in icon.tsx

--- spectify-frontend/src/app/icon.tsx
+++ spectify-frontend/src/app/icon.tsx
@@ -1,26 +1,30 @@
-import { ImageResponse } from "next/og";
 
-export const size = {
-    width: 32,
-    height: 32,
+import { NextApiRequest, NextApiResponse } from 'next';
+import ReactDOMServer from 'react-dom/server';
+
+const IconHandler = (req: NextApiRequest, res: NextApiResponse) => {
+	// Logic to generate and return the icon image
+	const icon = (
+		<div
+			style={{
+				fontSize: 24,
+				background: '',
+				width: '100%',
+				height: '100%',
+				display: 'flex',
+				alignItems: 'center',
+				justifyContent: 'center',
+				color: 'white',
+			}}
+		>
+			A
+		</div>
+	);
+
+	const iconMarkup = ReactDOMServer.renderToStaticMarkup(icon);
+
+	res.setHeader('Content-Type', 'image/png');
+	res.status(200).end(iconMarkup);
 };
-export const contentType = "image/png";
 
-const Icon = () => (
-    <div
-        style={{
-            fontSize: 24,
-            background: "",
-            width: "100%",
-            height: "100%",
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-            color: "white",
-        }}
-    >
-        A
-    </div>
-);
-
-export default Icon;
+export default IconHandler;
