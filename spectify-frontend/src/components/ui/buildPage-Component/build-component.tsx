4e67396a9b9131eeb15f82fd390ad24e07ad6da2
Thalit Chaimunin <112146489+time1568@users.noreply.github.com>
2024-05-27 17:11:00 +0700

    fix: issue
    Now forced user to select in order for every component.

--- spectify-frontend/src/components/ui/buildPage-Component/build-component.tsx
+++ spectify-frontend/src/components/ui/buildPage-Component/build-component.tsx
@@ -28,23 +28,6 @@ const BuildComponent: React.FC<BuildComponentProps> = ({ selectedProducts, setSe
     const isCpuSelected = selectedProducts["CPU"] !== null;
     const isMoboSelected = selectedProducts["MB"] !== null;
 
-    // const handleSelectProduct = (typeProduct: string, product: Product) => {
-    //     setSelectedProducts(prevProducts => {
-    //         const newProducts = { ...prevProducts, [typeProduct]: product };
-    //         localStorage.setItem('selectedProducts', JSON.stringify(newProducts));
-    //         localStorage.setItem(typeProduct, product.id);
-    
-    //         if (typeProduct === 'CPU' && 'socket' in product) {
-    //             const cpuProduct = product as cpuProducts;
-    //             setSelectedCpuSocket(cpuProduct.socket);
-    //         } else if (typeProduct === 'MB' && 'ramslot' in product) {
-    //             const moboProduct = product as unknown as moboProducts;
-    //             setSelectedMoboRamType(moboProduct.ramslot);
-    //         }
-
-    //         return newProducts;
-    //     });
-    // };
     const handleSelectProduct = (typeProduct: string, product: Product) => {
         setSelectedProducts(prevProducts => {
             const newProducts = { ...prevProducts, [typeProduct]: product };
@@ -67,6 +50,10 @@ const BuildComponent: React.FC<BuildComponentProps> = ({ selectedProducts, setSe
         });
     };
 
+    const selectionOrder = ['CPU', 'MB', 'RAM', 'GPU', 'SSD', 'PSU', 'Cooler', 'Case'];
+    const isComponentSelected = (index: number) => index === 0 || selectedProducts[selectionOrder[index - 1]] !== null;
+
+
     const handleDeselectProduct = (typeProduct: string) => {
         setSelectedProducts(prevProducts => {
             const newProducts = { ...prevProducts, [typeProduct]: null };
@@ -84,7 +71,7 @@ const BuildComponent: React.FC<BuildComponentProps> = ({ selectedProducts, setSe
     
             return newProducts;
         });
-    };
+    }; 
 
     const calculateTotalPrice = () => {
     
@@ -102,78 +89,20 @@ const BuildComponent: React.FC<BuildComponentProps> = ({ selectedProducts, setSe
 return (
     <>
         <div className='grid grid-cols-1 grid-rows-8 gap-4 place-items-center m-5'>
-            <ProductPopUp
-                typeProduct="CPU"
-                onSelectProduct={(product) => handleSelectProduct("CPU", product)}
-                onDeselectProduct={() => handleDeselectProduct("CPU")}
-                selectedProduct={selectedProducts["CPU"]}
-                selectedCpuSocket={null}
-                selectedMoboRamType={null}
-                disabled={false}  // CPU selection is never disabled
-            />
-            <ProductPopUp
-                typeProduct="MB"
-                onSelectProduct={(product) => handleSelectProduct("MB", product)}
-                onDeselectProduct={() => handleDeselectProduct("MB")}
-                selectedProduct={selectedProducts["MB"]}
-                selectedCpuSocket={selectedCpuSocket}
-                selectedMoboRamType={null}
-                disabled={!isCpuSelected}  // Disable MB selection until CPU is selected
-            />
-            <ProductPopUp
-                typeProduct="RAM"
-                onSelectProduct={(product) => handleSelectProduct("RAM", product)}
-                onDeselectProduct={() => handleDeselectProduct("RAM")}
-                selectedProduct={selectedProducts["RAM"]}
-                selectedCpuSocket={null}
-                selectedMoboRamType={selectedMoboRamType}
-                disabled={!isCpuSelected || !isMoboSelected}  // Disable RAM selection until CPU and MB are selected
-            />
-            <ProductPopUp
-                typeProduct="GPU"
-                onSelectProduct={(product) => handleSelectProduct("GPU", product)}
-                onDeselectProduct={() => handleDeselectProduct("GPU")}
-                selectedProduct={selectedProducts["GPU"]}
-                selectedCpuSocket={null}
-                selectedMoboRamType={null}
-                disabled={false}  // GPU selection is never disabled
-            />
-            <ProductPopUp
-                typeProduct="SSD"
-                onSelectProduct={(product) => handleSelectProduct("SSD", product)}
-                onDeselectProduct={() => handleDeselectProduct("SSD")}
-                selectedProduct={selectedProducts["SSD"]}
-                selectedCpuSocket={null}
-                selectedMoboRamType={null}
-                disabled={false}  // SSD selection is never disabled
-            />
-            <ProductPopUp
-                typeProduct="PSU"
-                onSelectProduct={(product) => handleSelectProduct("PSU", product)}
-                onDeselectProduct={() => handleDeselectProduct("PSU")}
-                selectedProduct={selectedProducts["PSU"]}
-                selectedCpuSocket={null}
-                selectedMoboRamType={null}
-                disabled={false}  // PSU selection is never disabled
-            />      
-            <ProductPopUp
-                typeProduct="Cooler"
-                onSelectProduct={(product) => handleSelectProduct("Cooler", product)}
-                onDeselectProduct={() => handleDeselectProduct("Cooler")}
-                selectedProduct={selectedProducts["Cooler"]}
-                selectedCpuSocket={null}
-                selectedMoboRamType={null}
-                disabled={false}  // Cooler selection is never disabled
-            />
-            <ProductPopUp
-                typeProduct="Case"
-                onSelectProduct={(product) => handleSelectProduct("Case", product)}
-                onDeselectProduct={() => handleDeselectProduct("Case")}
-                selectedProduct={selectedProducts["Case"]}
-                selectedCpuSocket={null}
-                selectedMoboRamType={null}
-                disabled={false}  // Case selection is never disabled
-            />
+            
+            {selectionOrder.map((typeProduct, index) => (
+                <ProductPopUp
+                    key={typeProduct}
+                    typeProduct={typeProduct}
+                    onSelectProduct={(product) => handleSelectProduct(typeProduct, product)}
+                    onDeselectProduct={() => handleDeselectProduct(typeProduct)}
+                    selectedProduct={selectedProducts[typeProduct]}
+                    selectedCpuSocket={typeProduct === 'MB' ? selectedCpuSocket : undefined}
+                    selectedMoboRamType={typeProduct === 'RAM' ? selectedMoboRamType : undefined}
+                    disabled={index > 0 && !isComponentSelected(index)}
+                />
+            ))}
+
             <div className='flex shadow-xl rounded-xl h-12 w-full text-center mt-3 bg-[#D9D9D9]'>
                 <h2 className='text-xl font-semibold flex justify-center my-2 mx-2'>
                     Total price: {totalPrice} Baht
